032c31932bd44fe47aadffd6e178ad650317f200
Jessica Churgin <jchurgy@gmail.com>
2025-06-30 15:58:41 -0400

    sample script to create folders and inner files

---
+++ Intro/boilerplate.js
@@ -0,0 +1,20 @@
+ 
+const fs = require('fs');
+//console.log(fs)
+const folderName = process.argv[2] || 'Project'
+
+// fs.mkdir('Cats', { recursive: true }, (err) => {
+//     console.log(`Within the callback function`)
+//     if (err) throw err
+//     console.log('Directory created successfully')
+// })
+
+// Using synchronous method to create a directory
+// this guarantees that the directory is created before the next line of code runs
+fs.mkdirSync(folderName, { recursive: true });
+
+fs.writeFileSync(`${folderName}/index.html`, '');
+fs.writeFileSync(`${folderName}/app.js`, '');
+fs.writeFileSync(`${folderName}/app.css`, '');
+
+console.log(`This is after the callback function`)
--- Intro/greeter.js
+++ Intro/greeter.js
@@ -3,3 +3,4 @@ const args = process.argv.slice(2)
 for(let arg of args) {
     console.log(`Hello, ${arg}!`)
 }
+
